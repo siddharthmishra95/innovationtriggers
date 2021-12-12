a8b9825bfce8b34ff6c3cfddafe40fc6015e5b92
siddharthmishra95 <siiid.mishra@gmail.com>
2021-12-12 14:29:01 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+    <!-- Jquery JS-->
+    <script src="<?php echo BASE_URL; ?>includes/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="<?php echo BASE_URL; ?>includes/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="<?php echo BASE_URL; ?>includes/vendor/slick/slick.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/wow/wow.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/animsition/animsition.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/counter-up/jquery.counterup.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>includes/vendor/select2/select2.min.js"></script>
+
+    <!-- Main JS-->
+    <script src="<?php echo BASE_URL; ?>includes/js/main.js"></script>
+
+    </body>
+
+    </html>
